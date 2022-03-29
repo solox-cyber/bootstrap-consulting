58ad49fd47990c92918e158fc4851bfccb2f77c9
Osadiaye Solomon <srose033@gmail.com>
2022-03-29 10:13:40 +0100

    recent

--- register.php
+++ register.php
@@ -1,3 +1,112 @@
+<?php 
+//session_start(); 
+//ob_start();
+
+ /*if submit button is hitting*/ 
+ if (isset($_POST['submit'])) 
+ {  $error=""; 
+//----------------------------------------- 
+ if (!empty($_POST['lastname'])) 
+ { $lastname = $_POST['lastname']; } 
+ else 
+ { $error .= "You didn't type in your lastname. <br />";  } 
+
+ //----------------------------------------- 
+  if (!empty($_POST['firstname'])) 
+ { $firstname = $_POST['firstname']; } 
+ else 
+ { $error .= "You didn't type in your firstname. <br />";  } 
+
+
+//----------------------------------------- 
+ if (!empty($_POST['gender'])) 
+ { $gender = $_POST['gender']; } 
+ else 
+ { $error .= "You didn't select your gender. <br />";  } 
+
+//----------------------------------------- 
+ if (!empty($_POST['employed'])) 
+ { $employed = $_POST['employed']; } 
+ else 
+ { $error .= "You didn't indicate if you Earn a Salary. <br />";  } 
+
+//----------------------------------------- 
+ if (!empty($_POST['sponsor'])) 
+ { $sponsor = $_POST['sponsor']; } 
+ else 
+ { $error .= "You didn't enter your Sponsors Name <br />";  } 
+
+//----------------------------------------- 
+ if (!empty($_POST['sponsorearns'])) 
+ { $sponsorearns = $_POST['sponsorearns']; } 
+ else 
+ { $error .= "You didn't enter  if your Sponsor Earn's a Salary <br />";  } 
+
+//----------------------------------------- 
+ if (!empty($_POST['program'])) 
+ { $program = $_POST['program']; } 
+ else 
+ { $error .= "You didn't enter the Program of Interest <br />";  } 
+ //----------------------------------------- 
+  if (!empty($_POST['phone'])) 
+ { $phone = $_POST['phone']; } 
+ else 
+ { $error .= "You didn't type in your phone number. <br />";  } 
+ //----------------------------------------- 
+if (!empty($_POST['email'])) 
+ {  $email = $_POST['email']; 
+if(!preg_match("/^[_a-z0-9]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i",
+ $email))
+ { $error .= "The e-mail address you entered is not valid. <br/>"; }
+ } 
+ else 
+ {  $error .= "You didn't type in an e-mail address. <br />"; } 
+
+//----------------------------------------- 
+ if (!empty($_POST['sponsorearns'])) 
+ { $sponsorearns = $_POST['sponsorearns']; } 
+ else 
+ { $error .= "You didn't enter  if your Sponsor Earn's a Salary <br />";  } 
+
+//----------------------------------------- 
+ if (!empty($_POST['comments'])) 
+ { $recruiter = $_POST['recruiter']; } 
+ else 
+ { $error .= "You didn't select Recuiter Code. Choose N/A if you don't have one <br />";  } 
+  if (empty($_POST['url'])) 
+{ 
+
+
+//----------------------------------------- 
+ if (!empty($_POST['comments'])) 
+ { $comments = $_POST['comments']; } 
+ else 
+ { $error .= "You didn't select Comments <br />";  } 
+
+
+//----------------------------------------- 
+//if(($_POST['code']) == $_SESSION['code']) 
+//{  $code = $_POST['code'];} 
+//else
+// {$error .= "The captcha code you entered does not match. Please try again. <br />" . $_SESSION['code'];}
+// test if error is empty mean we have nothing to send else we send the
+ if (empty($error)) 
+{ 
+ $from = "From: " . $firstname . " " . $lastname . " <" . $email . ">"; 
+$to = "info@bootstrapconsulting.net";
+// $to = "damiokuneye@yahoo.com";
+ $subject = "Financing EOI form"; 
+ $content = "Last Name: " . $lastname . " \n First Name: " . $firstname . "\n Gender: " . $gender . "\n Salary Earner?: " . $employed . "\n Sponsor Name: " . $sponsor . "\n Sponsor Earns?: " . $sponsorearns . "\n Program: " . $program . "\n Phone: " . $phone . "\n Email: " . $email . "\n Recruiter Code: " . $recruiter . "\n Comments: " . $comments;
+
+ $success = "<h3>Thank you! Your details have been sent.</h3> <br/>"; 
+ mail($to,$subject,$content,$from); 
+echo $success; } 
+ else { echo '<p class="error"><strong>Your details have  not been sent<br/> 
+ The following error(s) occurred:</strong><br/>' . $error . ' Please retry.</p>'; 
+} 
+} 
+ }
+?> 
 <!DOCTYPE html>
 <html lang="en">
 
@@ -127,17 +236,17 @@ Applicants must have a Sponsor who is a Salary Earner (or has a verifiable means
                
             </div>
             <div class="col-lg-6 py-6 px-5">
-                <form>
+                <form id="contact-form" method="post" enctype="multipart/form-data" action="register.php">
                     <div class="row g-3">
                         <div class="col-6">
                             <div class="form-floating">
-                                <input type="text" class="form-control" id="form-floating-1" placeholder="John Doe">
+                                <input type="text" name="lastname"  class="form-control" id="form-floating-1" placeholder="John Doe">
                                 <label for="form-floating-1">Last Name</label>
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="form-floating">
-                                <input type="text" class="form-control" id="form-floating-1" placeholder="John Doe">
+                                <input type="text" class="form-control" name="firstname"  id="form-floating-1" placeholder="John Doe">
                                 <label for="form-floating-1">First Name</label>
                             </div>
                         </div>
@@ -170,18 +279,18 @@ Applicants must have a Sponsor who is a Salary Earner (or has a verifiable means
 
                         <div class="col-6">
                             <div class="form-floating">
-                                <input type="email" class="form-control" id="form-floating-2" placeholder="">
-                                <label for="form-floating-2">Sponsor Name</label>
+                                <input type="text" class="form-control"  name="sponsor"  id="form-floating-2" placeholder="">
+                                <label for="form-floating-2">Sponsor Name (Specify N/A if you don't require one:</label>
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="form-floating">
-                                <select name="program" class="form-control" id="select">
-                                    <option value="Cyber Security" selected>Cyber Security</option>
-                                    <option value="GRC">GRC</option>
-<option value=" IT Business Analysis">IT Business Analysis</option>
-<option value="UI/UX">UIUX</option>
-                                     </select>
+                                <select name="sponsorearns"  class="form-control" id="select">
+                                    <option value="No" selected>No</option>
+                                    <option value="Yes">Yes</option>
+                                    <option value="No,  but sponsor has a verifiable means of regular income">No,  but sponsor has a verifiable means of regular income</option>
+                             <option value="Not Applicable">Not Applicable</option>
+                            </select>
                                 <label for="form-floating-3">Sponsor Earns?</label>
                             </div>
                         </div>
@@ -200,7 +309,7 @@ Applicants must have a Sponsor who is a Salary Earner (or has a verifiable means
                         
                         <div class="col-6">
                             <div class="form-floating">
-                                <input type="email" class="form-control" id="form-floating-2" placeholder="">
+                                <input type="text" class="form-control" name="phone"  id="form-floating-2" placeholder="">
                                 <label for="form-floating-2">Phone Number</label>
                             </div>
                         </div>
@@ -218,12 +327,13 @@ Applicants must have a Sponsor who is a Salary Earner (or has a verifiable means
 
                         <div class="col-12">
                             <div class="form-floating">
-                                <textarea class="form-control" placeholder="Message" id="form-floating-4" style="height: 150px"></textarea>
+                                <input name="url" type="hidden">
+                                <textarea class="form-control" name="comments" placeholder="Message" id="form-floating-4" style="height: 150px"></textarea>
                                 <label for="form-floating-4">Comments</label>
                               </div>
                         </div>
                         <div class="col-12">
-                            <button class="btn btn-primary w-100 py-3" type="submit">Submit</button>
+                            <button class="btn btn-primary w-100 py-3" name="submit" type="submit">Submit</button>
                         </div>
                     </div>
                 </form> 
